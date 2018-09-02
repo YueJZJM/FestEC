404337196c65091523cf217e840f1ee910073f7b
YueJZJM <yuejzhfut@163.com>
2018-09-02 22:23:12 +0800

    fitrst

---
+++ latte-core/src/main/java/com/example/latte_core/app/Configurator.java
@@ -0,0 +1,48 @@
+package com.example.latte_core.app;
+
+import android.print.PrinterId;
+import android.support.v4.widget.NestedScrollView;
+
+import java.util.WeakHashMap;
+
+public class Configurator {
+    private static final WeakHashMap<String,Object> LATTE_CONFIGS = new WeakHashMap<>();
+
+    private Configurator(){
+        LATTE_CONFIGS.put(ConfigType.CONFIG_READY.name(),false);
+    }
+
+    public static Configurator getInstance(){
+        return Holder.INSTANCE;
+    }
+
+    final WeakHashMap<String,Object> getLatteConfigs(){
+        return LATTE_CONFIGS;
+    }
+    //单例，线程安全的懒汉模式
+    private static class Holder{
+        private static final Configurator INSTANCE = new Configurator();
+    }
+
+    public final void configure(){
+        LATTE_CONFIGS.put(ConfigType.CONFIG_READY.name(),true);
+    }
+
+    public final Configurator withApiHost(String host){
+        LATTE_CONFIGS.put(ConfigType.API_HOST.name(),host);
+        return this;
+    }
+
+    private void checkConfiguration(){
+        final boolean isReady = (boolean) LATTE_CONFIGS.get(ConfigType.CONFIG_READY.name());
+        if (!isReady){
+            throw new RuntimeException("Configuration is not read,call config");
+        }
+    }
+    //@SuppressWarnings("uncheched")
+    @SuppressWarnings("unchecked")
+    final <T> T getConfiguration(Enum<ConfigType> key){
+        checkConfiguration();
+        return (T) LATTE_CONFIGS.get(key.name());
+    }
+}
