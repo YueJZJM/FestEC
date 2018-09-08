e0a851046798f073e9f1bf0d1baec3c47add441f
YueJZJM <yuejzhfut@163.com>
2018-09-09 01:14:00 +0800

    完成网络框架,对应5.10

---
+++ latte-ec/src/main/java/com/example/latte/ec/launcher/LauncherDelegate.java
@@ -0,0 +1,90 @@
+package com.example.latte.ec.launcher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.latte.ec.R;
+import com.example.latte.ec.R2;
+import com.example.latte_core.app.Latte;
+import com.example.latte_core.delegates.LatteDelegate;
+import com.example.latte_core.ui.launcher.ScrollLauncherTag;
+import com.example.latte_core.util.storeage.LattePreference;
+import com.example.latte_core.util.timer.BaseTimerTask;
+import com.example.latte_core.util.timer.ITimerListerer;
+
+import java.text.MessageFormat;
+import java.util.Timer;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class LauncherDelegate extends LatteDelegate implements ITimerListerer{
+
+    @BindView(R2.id.tv_launcher_timer)
+    AppCompatTextView mTvTimer = null;
+
+    private int mCount = 5;
+    private Timer mTimer;
+
+    @OnClick(R2.id.tv_launcher_timer)
+    void onClickTimerView(){
+        if (mCount < 0){
+            if (mTimer != null){
+                mTimer.cancel();
+                mTimer = null;
+                checkIsShowScroll();
+             //   Toast.makeText(Latte.getApplicationContext(),"")
+            }
+        }
+    }
+
+    private void initTimer(){
+        mTimer = new Timer();
+        final BaseTimerTask task = new BaseTimerTask(this);
+        //0表示延迟的时间，1000表示间隔的时间
+        mTimer.schedule(task,0,1000);
+    }
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_launcher;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        initTimer();
+    }
+
+    private void checkIsShowScroll(){
+        if (!LattePreference.getAppFlag(ScrollLauncherTag.HAS_FIRST_LAUNCHER_APP.name())){
+            start(new LauncherScrollDelegate(),SINGLETASK);
+        }else {
+            //检查用户是否登陆
+
+        }
+    }
+
+
+    @Override
+    public void onTimer() {
+        getProxyActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mTvTimer != null){
+                    mTvTimer.setText(MessageFormat.format("跳过\n{0}s",mCount));
+                    mCount--;
+                    if (mCount < 0){
+                        if (mTimer != null){
+                            mTimer.cancel();
+                            mTimer = null;
+                            checkIsShowScroll();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
